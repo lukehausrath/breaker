cb81b51fca486039948aa7f4070c9381eabd8832
Lucas <lhausrath@gmail.com>
2017-01-10 04:46:53 -0800

    Score Tracking
    Added simple score which scales based on # of balls in play

--- Scripts/GameScene/BallNode.gd
+++ Scripts/GameScene/BallNode.gd
@@ -12,15 +12,29 @@ var redBall = preload("res://assets/PNG/marbles/Red.png")
 var tealBall = preload("res://assets/PNG/marbles/Teal.png")
 var yellowBall = preload("res://assets/PNG/marbles/Yellow.png")
 
+var scoreNode
+var previousModifier = 1
+
 func _ready():
 	set_process(true)
+	
+	scoreNode = get_node("/root/Node2D/Score")
+	assert(scoreNode != null)
 	
 	var paddle = get_node("/root/Node2D/PaddleKinematicBody")
 	paddle.connect("Launched", self, "set_launched")
 
 func _process(delta):
-	if (launched && !get_child_count()):
+	var childCount = get_child_count()
+	if (launched && !childCount):
 		get_parent().titleScene()
+	
+	if (childCount != previousModifier):
+		scoreNode.emit_signal("score_modifier", childCount)
+		previousModifier = childCount
 
 func set_launched():
 	launched = true
+
+func scoreIncrease():
+	scoreNode.emit_signal("increase_score", 5)
